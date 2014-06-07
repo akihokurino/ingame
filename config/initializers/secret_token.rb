58df34c166852950838b1b8a86045c47df1803bd
akihokurino <a_k_i_h_o@i.softbank.jp>
2014-06-08 01:16:19 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ingame::Application.config.secret_key_base = '36581ad3eb070e37bcbc4303d8dfd8975af464c4422d7d9f545cede65a6c0c3031ad1b610a6dde3a3e967173e30e6fbac090091aadc20fc8144a1e98b3cb3490'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ingame::Application.config.session_store :cookie_store, key: '_ingame_session'
