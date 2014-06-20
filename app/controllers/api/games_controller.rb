e02b3cb7d33dbfc0b5f743f9c2eec12082157213
akihokurino <a_k_i_h_o@i.softbank.jp>
2014-06-20 12:01:34 +0900

    投稿時のゲームデータを自分のログから取得するように修正

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,5 +1,7 @@
 class Api::GamesController < ApplicationController
 	def index
-		@games = Game.all.select(:id, :title)
+		@games = Log.where(user_id: @current_user[:id]).select(:game_id).map do |log|
+			log.game.select(:id, :title)
+		end
 	end
 end
