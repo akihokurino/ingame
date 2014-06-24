6434468648fe044f89836f080085a5666241ca5e
akihokurino <a_k_i_h_o@i.softbank.jp>
2014-06-24 13:57:31 +0900

    gameのLikeのapi

--- app/models/game_like.rb
+++ app/models/game_like.rb
@@ -8,4 +8,17 @@ class GameLike < ActiveRecord::Base
 	validates :user_id,
 		presence: true,
 		numericality: true
+
+	class << self
+		def check_and_create(game_like_params)
+			return false if self.exists?(game_like_params)
+			self.create(game_like_params) ? true : false
+		end
+
+		def check_and_destroy(param_hash)
+			return false unless self.exists?(param_hash)
+			game_like = self.find_by(param_hash)
+			game_like.destroy ? true : false
+		end
+	end
 end
