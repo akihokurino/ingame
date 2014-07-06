92156a39495510b5ca06969bb0cdb8fcee901407
smokycat <smokycat@fizzitter.com>
2014-07-07 02:52:31 +0900

    playdy.net -> playdy.dev  というか、ドメイン取れないね。厄介だ。

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     # 悪用禁止。開発期間終わったら、このAppは閉じます
-    # 1. 127.0.0.1をplaydy.netにする（/etc/hosts）
-    # 2. playdy.net:3000/users/login からtwitterでログインする。
+    # 1. 127.0.0.1をplaydy.devにする（/etc/hosts）
+    # 2. playdy.dev:3000/users/login からtwitterでログインする。
   	# provider :twitter, "Consumer key", "Consumer secret"
   	provider :twitter, "rfKeEqmpWn1KWj6XKoELR54YA", "iUGETGMiVp4qCMRM9eK4H011pBdUtslVgSUPsbyZXfNUTAzOXU"
 
