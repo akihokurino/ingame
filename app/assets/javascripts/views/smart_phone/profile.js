2a93844daed8b9ce89b9a1a219214cc553008f20
akihokurino <a_k_i_h_o@i.softbank.jp>
2014-10-03 01:48:35 +0900

    profile.jsのリファクタ

--- app/assets/javascripts/views/smart_phone/profile.js
+++ app/assets/javascripts/views/smart_phone/profile.js
@@ -3,19 +3,11 @@
 
 (function () {
 	$(function () {
-		var user_id = $(".profile-page").data("userid");
-
-
-		/* ---------- Collection --------- */
-		var logs    = new Logs();
-
-
 
 		/* ---------- View ---------- */
 		var LogsView = Backbone.View.extend({
 			el: $(".log-list"),
 			initialize: function () {
-				this.collection = logs;
 				this.listenTo(this.collection, "add", this.addLog);
 			},
 			addLog: function (log) {
@@ -42,6 +34,7 @@
 			render: function () {
 				var template = this.template(this.model.toJSON());
 				this.$el.html(template);
+
 				return this;
 			}
 		})
@@ -58,7 +51,8 @@
 			},
 			initialize: function () {
 				var that               = this;
-				this.logs_view         = new LogsView();
+				this.log_collection    = new Logs();
+				this.logs_view         = new LogsView({collection: this.log_collection});
 
 				this.attentions        = [];
 				this.playings          = [];
@@ -67,16 +61,15 @@
 				this.follow_template   = _.template($("#follow-template").html());
 				this.unfollow_template = _.template($("#unfollow-template").html());
 
-				this.search_log_title  = $(".search-log");
+				this.search_log_title  = this.$(".search-log");
 				this.current_tab       = null;
+				this.user_id           = this.$el.data("userid");
 
-				$.ajax({
-					type: "GET",
-					url: "/api/logs?user_id=" + user_id,
-					data: {},
-					success: function (data) {
-						for (var i = 0; i < data.logs.length; i++) {
-							var log = new Log(data.logs[i]);
+				this.log_collection.fetch({
+					data: {user_id: this.user_id},
+					success: function (model, response, options) {
+						for (var i = 0; i < response.logs.length; i++) {
+							var log = new Log(response.logs[i]);
 							switch (log.get("status").id) {
 								case 1:
 									that.attentions.push(log);
@@ -93,7 +86,7 @@
 						}
 					},
 					error: function () {
-						console.log("error");
+
 					}
 				})
 			},
@@ -131,7 +124,7 @@
 				var that = this;
 				var data = {
 					"follow": {
-						"to_user_id": user_id
+						"to_user_id": this.user_id
 					}
 				}
 
@@ -140,13 +133,13 @@
 					url: "/api/follows",
 					data: data,
 					success: function (data) {
-						if(data.result){
+						if (data.result) {
 							that.$el.find(".follow-wrap").html("");
 							that.$el.find(".follow-wrap").append(that.unfollow_template);
 						}
 					},
 					error: function () {
-						console.log("error");
+
 					}
 				})
 			},
@@ -154,7 +147,7 @@
 				var that = this;
 				$.ajax({
 					"type": "DELETE",
-					url: "/api/follows/" + user_id,
+					url: "/api/follows/" + this.user_id,
 					data: {},
 					success: function (data) {
 						if (data.result) {
@@ -163,13 +156,14 @@
 						}
 					},
 					error: function () {
-						console.log("error");
+
 					}
 				})
 			},
 			search: function (e) {
-				e.preventDefault();
 				if (e.which == 13 && this.search_log_title.val() != "") {
+					e.preventDefault();
+
 					this.logs_view.collection.reset();
 					this.logs_view.removeLogs();
 					this.$el.find("ul.sortBox li").removeClass("current");
