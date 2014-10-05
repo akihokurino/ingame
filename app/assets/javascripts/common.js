38c19d2b3d87c2496fe0830462f09aa2f662801c
Takahiro <rapo_rhythm@yahoo.co.jp>
2014-10-05 11:45:33 +0900

    [SP版プロフ画像] 円形のプロフ画像の対応途中

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -45,5 +45,41 @@ $(function(){
     }
   })();
   var Menu = APP.UI.menu;
+
+  APP.UI.profImg = (function(){
+    var _img = new Image(),
+        _profImg = $('.profile-page .personalBoxWrap .profImg'),
+        _profImgWrap = $('.personalBoxWrap .profImgWrap'),
+        _profImgWidth,
+        _profImgHeight;
+
+    //CSSを更新
+    function _centerPos(){
+      _profImg.css({
+        'margin-top' : '-' + (_profImgHeight * 0.5) + 'px',
+        'margin-left' : '-' + (_profImgWidth * 0.5) + 'px'
+      });
+    }
+    //プロフィール画像のサイズを取得
+    function _getImgSize(){
+      _profImg.on('load', function(){
+        _img.src = _profImg.attr('src');
+        _profImgWidth = _img.width;
+        _profImgHeight = _img.height;
+      });
+      console.log(_img.width);
+    }
+    //初期化
+    function _init(){
+      _getImgSize();
+      _centerPos();
+    }
+    return {
+      init: _init
+    }
+  })();
+  var ProfImg = APP.UI.profImg;
+
   Menu.init();
+  ProfImg.init();
 });
