4cb73a55cc6f5b220148f025a80df64ee6c9d6cb
Takahiro <rapo_rhythm@yahoo.co.jp>
2014-10-11 19:01:47 +0900

    [PC版テンプレート] 一時バックアップ

---
+++ template/config.rb
@@ -0,0 +1,10 @@
+Encoding.default_external = "utf-8"
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "stylesheets"
+images_dir = "images"
+javascripts_dir = "js"
+#output_style = :expanded
+output_style = :compressed
+line_comments = false
+cache_dir = "../.sass-cache"
