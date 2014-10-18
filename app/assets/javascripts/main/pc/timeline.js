78e5283cca5dba656dc00bb013e948195bd3e90b
akihokurino <a_k_i_h_o@i.softbank.jp>
2014-10-18 15:17:43 +0900

    commonもう一回移す

--- app/assets/javascripts/main/pc/timeline.js
+++ app/assets/javascripts/main/pc/timeline.js
@@ -1,31 +1,26 @@
 //= require ../../libs/socket.js
 //= require ../../models/post.js
-//= require ../../models/comment.js
 //= require ../../collections/posts.js
-//= require ../../collections/comments.js
 //= require ../../views/post_view.js
 //= require ../../views/posts_view.js
-//= require ../../views/comment_view.js
-//= require ../../views/comments_view.js
+
 
 
 (function () {
   var AppView = Backbone.View.extend({
     el: ".timeline-page",
     events: {
-      "click .cancel-modal-btn":   "hideComment",
-      "click .submit-comment-btn": "postComment"
+      "click .submit-comment-btn": "postComment",
+      "click .show-select-modal": "toggleSelectModal"
     },
     initialize: function () {
-      _.bindAll(this, "pagenation", "showComment");
+      _.bindAll(this, "pagenation");
 
       var that                = this;
       this.post_collection    = new Posts();
-      this.comment_collection = null;
       this.posts_view         = new PostsView({collection: this.post_collection});
-      this.comments_view      = new CommentsView({collection: this.comment_collection});
-      this.comment_input      = this.$(".comment-input");
 
+      this.comment_input      = this.$(".comment-input");
       this.page               = 1;
 
       event_handle.discribe("showComment", this.showComment);
@@ -55,6 +50,7 @@
         that.posts_view.$el.prepend(post_view.render().el);
       }
 
+      /*
       comment_socket.callback = function (data) {
         that.post_collection.find(function (model) {
           if (model.id == data.post_id) {
@@ -71,6 +67,7 @@
           }
         });
       }
+      */
 
 
       this.post_collection.fetch({
@@ -117,61 +114,11 @@
         })
       }
     },
-    showComment: function (model) {
-      this.commented_post_model = model;
-      this.comment_collection   = new Comments(model.get("post_comments"));
-      this.comments_view        = new CommentsView({collection: this.comment_collection});
-
-      $(".comment-modal").css("display", "block");
-      $(".layer").css("display", "block");
-    },
-    hideComment: function () {
-      this.commented_post_model = null;
-      this.comment_collection   = null;
-      this.comments_view        = null;
-
-      this.comment_input.val("");
-
-      $(".comment-modal").css("display", "none");
-      $(".layer").css("display", "none");
-    },
-    postComment: function () {
-      if (this.comment_input.val() != "") {
-        var that = this;
-
-        var data = {
-          "post_comment": {
-            "post_id":    this.commented_post_model.id,
-            "text":       this.comment_input.val(),
-            "to_user_id": this.commented_post_model.get("user").id
-          }
-        }
-
-        this.comment_collection.create(data, {
-          method: "POST",
-          success: function (response) {
-            var comment = new Comment(response.get("comment"));
-            that.comments_view.collection.add(comment);
-            that.commented_post_model.get("post_comments").push(response.get("comment"));
-            that.commented_post_model.set("post_comments_count", that.commented_post_model.get("post_comments_count") + 1);
-
-            that.comment_input.val("");
-
-            var data = {
-              type: "comment",
-              comment: response.get("comment"),
-              post_id: that.commented_post_model.id,
-              from_user_id: comment_socket.user_id,
-              to_user_id: that.commented_post_model.get("user").id
-            }
-
-            comment_socket.send(data);
-
-          },
-          error: function () {
-
-          }
-        })
+    toggleSelectModal: function () {
+      if ($(".selectModal").css("display") == "none") {
+        $(".selectModal").css("display", "block");
+      } else {
+        $(".selectModal").css("display", "none");
       }
     }
   })
