e2e51936df9c885f9babad5e8f835aae0e98e641
akihokurino <a_k_i_h_o@i.softbank.jp>
2014-10-23 14:09:14 +0900

    シェルの更新

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,7 +1,3 @@
 rake db:migrate:reset
-rake db:seed
-rake db:fixtures:load FIXTURES=users
-rake db:fixtures:load FIXTURES=posts
-rake db:fixtures:load FIXTURES=logs
-rake db:fixtures:load FIXTURES=notifications
+
 
