fc2f9dc735703176483bca03c8d4f646e7297f8e
akihokurino <a_k_i_h_o@i.softbank.jp>
2014-12-06 23:03:37 +0900

    シェル追加

---
+++ shell/make_folders.sh
@@ -0,0 +1,10 @@
+cd public
+mkdir game_photos
+chmod 777 game_photos
+mkdir user_photos
+chmod 777 user_photos
+mkdir tmp_photos
+chmod 777 tmp_photos
+mkdir post_photos
+chmod 777 post_photos
+cd ../
