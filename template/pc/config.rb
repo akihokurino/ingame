1a17bafab0f5e39a2fe10e442478d6149b156275
Takahiro <rapo_rhythm@yahoo.co.jp>
2014-12-06 14:15:02 +0900

    [PC] アカウント登録フローのスタイル充て（途中）

---
+++ template/pc/config.rb
@@ -0,0 +1,10 @@
+Encoding.default_external = "utf-8"
+http_path = "/"
+css_dir = "./"
+sass_dir = "./"
+images_dir = "./"
+javascripts_dir = "./"
+#output_style = :expanded
+output_style = :compressed
+line_comments = false
+cache_dir = "./.sass-cache"
