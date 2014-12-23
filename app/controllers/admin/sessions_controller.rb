ac664084bf90ff3e982efc6a71f448b43b2f57f9
akihokurino <a_k_i_h_o@i.softbank.jp>
2014-12-23 18:44:07 +0900

    basic認証もかける

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,8 +1,10 @@
 class Admin::SessionsController < ApplicationController
+  require 'digest'
   layout "application_admin"
 
   skip_before_action :auth
   before_action :auth_admin, only: [:destroy]
+  before_action :basic_auth, only: [:new, :create]
 
   def new
   end
@@ -10,12 +12,23 @@ class Admin::SessionsController < ApplicationController
   def create
     if admin = Admin.authenticate(params[:username], params[:password], params[:key])
       session[:current_admin_id] = admin[:id]
-      redirect_to admin_games_path, notice: "ログインに成功しました。ようこそ管理画面へ。"
+      redirect_to admin_games_path
     else
       redirect_to new_admin_session_path, alert: "不正なユーザーです。"
     end
   end
 
   def destroy
+    reset_session
+    redirect_to new_admin_session_path, alert: "正常にログアウトしました。"
+  end
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      md5_of_password = Digest::MD5.hexdigest(password)
+      username == 'ingame_admin' && md5_of_password == '5ebe2294ecd0e0f08eab7690d2a6ee69'
+      # password == secret
+    end
   end
 end
