53a66ce28d9abd9afb28f67a34c71f0d37384bcf
akihokurino <a_k_i_h_o@i.softbank.jp>
2014-12-24 19:15:00 +0900

    gameの編集

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -3,7 +3,7 @@ class Admin::GamesController < ApplicationController
 
   skip_before_action :auth
   before_action :auth_admin
-  before_action :set_game, only: [:show, :edit, :update]
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
 
   PER = 50
 
@@ -18,16 +18,29 @@ class Admin::GamesController < ApplicationController
   end
 
   def update
+    if @game.update game_params
+      redirect_to admin_game_path(@game), notice: "正常に編集が完了しました。"
+    else
+      redirect_to edit_admin_game_path(@game), alert: "編集ができませんでした。"
+    end
   end
 
   def new
+    @game = Game.new
   end
 
   def create
   end
 
+  def destroy
+  end
+
   private
   def set_game
     @game = Game.find params[:id]
+  end
+
+  def game_params
+    params.require(:game).permit :title, :photo_path, :maker, :wiki, :price, :release_day, :device, :provider, :provider_url, :amazon_url
   end
 end
