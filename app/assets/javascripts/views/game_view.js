5f889903e9d9f757d38c5278c46075608e87e3fc
Takahiro <rapo_rhythm@yahoo.co.jp>
2015-01-04 22:56:53 +0900

    [PC] gamelistリファクタリング

--- app/assets/javascripts/views/game_view.js
+++ app/assets/javascripts/views/game_view.js
@@ -1,6 +1,6 @@
 var GameView = Backbone.View.extend({
   tagName: "div",
-  className: "gameBox",
+  className: "Gamelist",
   template: _.template($("#game-template").html()),
   render: function () {
     var template = this.template(this.model.toJSON());
