e19370c6b5f302fbc3f3fdbc219be2842f9448bb
Takahiro <rapo_rhythm@yahoo.co.jp>
2015-01-04 23:12:06 +0900

    [PC] gamelist修正

--- app/assets/javascripts/views/game_view.js
+++ app/assets/javascripts/views/game_view.js
@@ -1,6 +1,6 @@
 var GameView = Backbone.View.extend({
   tagName: "div",
-  className: "Gamelist",
+  className: "gameBox",
   template: _.template($("#game-template").html()),
   render: function () {
     var template = this.template(this.model.toJSON());
