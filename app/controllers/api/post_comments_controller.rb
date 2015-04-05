3afd71e9060eebdb1886f4049c444927d62fa701
akihokurino <a_k_i_h_o@i.softbank.jp>
2015-04-05 14:15:39 +0900

    ポスト詳細から通知が飛ばないバグの修正

--- app/controllers/api/post_comments_controller.rb
+++ app/controllers/api/post_comments_controller.rb
@@ -7,8 +7,9 @@ class Api::PostCommentsController < ApplicationController
   end
 
   def create
-    params[:post_comment][:user_id] = @current_user[:id]
-    @comment                        = PostComment.create post_comment_params
+    params[:post_comment][:user_id]    = @current_user[:id]
+    @comment                           = PostComment.create post_comment_params
+    params[:post_comment][:to_user_id] = Post.find(params[:post_comment][:post_id]).user_id if params[:post_comment][:to_user_id].blank?
 
     unless @current_user[:id].to_i == params[:post_comment][:to_user_id].to_i
       Notification.create from_user_id: @current_user[:id], to_user_id: params[:post_comment][:to_user_id], notification_type_id: 3, post_id: @comment[:post_id]
--- app/controllers/api/post_likes_controller.rb
+++ app/controllers/api/post_likes_controller.rb
@@ -1,12 +1,8 @@
 class Api::PostLikesController < ApplicationController
 	def create
-		params[:post_like][:user_id] = @current_user[:id]
-		@result                      = PostLike.check_and_create post_like_params
-
-		unless params[:post_like][:to_user_id]
-			post = Post.find params[:post_like][:post_id]
-			params[:post_like][:to_user_id] = post[:user_id]
-		end
+		params[:post_like][:user_id]    = @current_user[:id]
+		@result                         = PostLike.check_and_create post_like_params
+		params[:post_like][:to_user_id] = Post.find(params[:post_like][:post_id]).user_id if params[:post_like][:to_user_id].blank?
 
 		unless @current_user[:id].to_i == params[:post_like][:to_user_id].to_i
 			Notification.create from_user_id: @current_user[:id], to_user_id: params[:post_like][:to_user_id], notification_type_id: 2, post_id: params[:post_like][:post_id]
