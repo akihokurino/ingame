568d7fe15b48368601b7bf1c83798b472485cd32
akihokurino <aki030402@gmail.com>
2015-05-24 21:25:35 +0900

    game_resultのリファクタ

--- app/assets/javascripts/models/game_result.js
+++ app/assets/javascripts/models/game_result.js
@@ -16,5 +16,52 @@ var GameResult = Backbone.Model.extend({
       new_value    += "...";
       this.set(prop, new_value);
     }
-  }
+  },
+  toggleRegist: function (status_id, regist_callback, unregist_callback) {
+    var that = this;
+    if (this.get("i_registed")) {
+      var data = {
+        "log": {
+          "status_id": status_id
+        }
+      }
+
+      $.ajax({
+        type: "PUT",
+        url: "/api/logs/" + this.id + "/update_status_or_rate",
+        data: data,
+        success: function (data) {
+          if (regist_callback) {
+            regist_callback();
+          }
+        },
+        error: function () {
+
+        }
+      });
+    } else {
+      var data = {
+        "log": {
+          "game_id":   this.id,
+          "status_id": status_id
+        }
+      }
+
+      $.ajax({
+        type: "POST",
+        url: "/api/logs",
+        data: data,
+        success: function (data) {
+          that.set("i_registed", true);
+
+          if (unregist_callback) {
+            unregist_callback();
+          }
+        },
+        error: function () {
+
+        }
+      });
+    }
+  },
 });
--- app/assets/javascripts/views/game_result_view.js
+++ app/assets/javascripts/views/game_result_view.js
@@ -20,47 +20,10 @@ var GameResultView = Backbone.View.extend({
   regist: function () {
     if (this.$el.find(".my-status").val() != "") {
       var that = this;
-
-      if (this.model.get("i_registed")) {
-        var data = {
-          "log": {
-            "status_id": this.$el.find(".my-status").val()
-          }
-        }
-
-        $.ajax({
-          type: "PUT",
-          url: "/api/logs/" + this.model.id + "/update_status_or_rate",
-          data: data,
-          success: function (data) {
-
-          },
-          error: function () {
-
-          }
-        });
-      } else {
-        var data = {
-          "log": {
-            "game_id":   this.model.id,
-            "status_id": this.$el.find(".my-status").val()
-          }
-        }
-
-        $.ajax({
-          type: "POST",
-          url: "/api/logs",
-          data: data,
-          success: function (data) {
-            that.model.set("i_registed", true);
-            that.$el.addClass("registed");
-            $(".next-page").html("次へ");
-          },
-          error: function () {
-
-          }
-        });
-      }
+      this.model.toggleRegist(this.$el.find(".my-status").val(), null, function () {
+        that.$el.addClass("registed");
+        $(".next-page").html("次へ");
+      });
     }
   },
   setCurrentStatus: function () {
